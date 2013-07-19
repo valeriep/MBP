69ff5c8668eb545fbedf4718e27af4b72769ac8a
pepoz <pepoz@192.168.1.69>
2013-07-19 11:10:34 +0200

    evolution pour liste piste

--- MBP/assets/www/js/controleur/initPage.js
+++ MBP/assets/www/js/controleur/initPage.js
@@ -8,35 +8,32 @@ function init(){
 			alert("pas de connexion internet");
 		}
 		else {
-			
+
 			if(navigator.geolocation){
 				navigator.geolocation.getCurrentPosition(onSuccess, onError);
-				getPisteList(latitude, longitude);
-				//initbdd();
 			}
 
 			else{
 				alert('no geolocation support');
 			}
 		}
+		//afficheListePiste(pisteAAfficher);
 	});
 
 }
 
+// Fonction de callback onSuccess, reçoit un objet Position
+//
+function onSuccess(position) {
+	alert("votre position : longitude " + position.coords.longitude +"\nlatitude : " + position.coords.latitude);
+	var listPisteSeolan = getPisteList(latitude, longitude);
+	initbdd(listPisteSeolan);
+	//getPisteList(position.coords.latitude, position.coords.longitude);
+}
 
+//Fonction de callback onError, reçoit un objet PositionError
 
-	// Fonction de callback onSuccess, reçoit un objet Position
-	//
-	function onSuccess(position) {
-		alert("aa");
-		//alert("votre position : longitude " + position.coords.longitude +"\nlatitude : " + position.coords.latitude);
-		//getPisteList(position.coords.latitude, position.coords.longitude);
-	}
-
-	// Fonction de callback onError, reçoit un objet PositionError
-	//
-	function onError(error) {
-		alert('cest une erreur');
-		alert('code erreur geolocalisation : '    + error.code    + '\n' +
-				'message : ' + error.message + '\n');
-	}
+function onError(error) {
+	alert('code erreur geolocalisation : '    + error.code    + '\n' +
+			'message : ' + error.message + '\n');
+}
