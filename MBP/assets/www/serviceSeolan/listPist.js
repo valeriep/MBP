e06ea7ed1f8b2adac2bebbe7f36cfb99ad9e3efa
pepoz <pepoz@192.168.1.69>
2013-07-22 11:31:01 +0200

    update pour ne faire que recuperer les pistes

--- MBP/assets/www/serviceSeolan/listPist.js
+++ MBP/assets/www/serviceSeolan/listPist.js
@@ -16,45 +16,5 @@ function getPisteList(latitude, longitude) {
 	   
 	    return pistesSeolan;
 	
-  
-
-	
-//	$.getJSON(serviceURL,  { latitude: latitude, longitude: longitude })
-	   
-//	.done(function(data) {
-//		$('#liste_pistes li').remove();
-//		//valeurNote = '3.5';
-//		$.each(data, function(index, piste) {
-//			// Dans le cas ou pas image de piste sur le serveur
-//			if (!piste.F0001){
-//				imagePiste = "./images/pisteDefault.png";
-//			}
-//			else {
-//				imagePiste = domaine+ piste.F0001+ '&geometry=50x50%3E'; 
-//			}
-//		var divStar = "star" + index;
-//		
-//		$('#liste_pistes').append('<li><div class="piste"><div class="photo"><img src="' + imagePiste +'" alt="Piste" width="50px" height="50px"></div>' +
-//					'<div class="texte"><h2><a href="detailPiste.html?piste.idPiste=' + piste.oid + '">' + piste.nom + 
-//					'<div style="background-color:'+piste.couleurId+'" class="couleur img-circle"></div></h2></div>' +
-//					'<div class="note_globale">' + 
-//					'<span>Note </span> <strong>' + piste.notGlob + '</strong></div>' + 
-//					'<div id="' + divStar  + '" data-score="' + piste.notGlob + '" disabled="disabled">' +
-//					'</div></div>');
-//			$("#" + divStar ).raty({
-//				readOnly  : true,
-//				width: false,
-//				path : "./images/",
-//				score: function() {
-//				return $(this).attr('data-score');
-//				}
-//				});
-//			});
-//		$('#liste_pistes').listview('refresh');
-//	})
-//	.fail(function( jqxhr, textStatus, error ) {
-//var err = textStatus + ', ' + error;
-//
-//});
-
+ 
 }
