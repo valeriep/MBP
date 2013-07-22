fe8d99d21f8ca389b31ee69ada41949370fea3a2
pepoz <pepoz@192.168.1.69>
2013-07-22 11:24:10 +0200

    version avec affichage liste piste suite lecture piste BDD

--- MBP/assets/www/js/controleur/initPage.js
+++ MBP/assets/www/js/controleur/initPage.js
@@ -1,8 +1,8 @@
 var latitude;
 var longitude;
+var listDesPistes = new Array();
 
 function init(){
-	alert("ici");
 	document.addEventListener("deviceready", function(){
 
 		if(navigator.network.connection.type == Connection.NONE){
@@ -11,18 +11,23 @@ function init(){
 		else {
 
 			if(navigator.geolocation){
-				navigator.geolocation.getCurrentPosition(onSuccess, onError);
+				alert("dans geoloc");
+				navigator.geolocation.getCurrentPosition(creationBDD, onError);
 			}
 
 			else{
 				alert('no geolocation support');
+				listPistAll();
 			}
 		}
-		//afficheListePiste(pisteAAfficher);
+			
+		// affichage des pistes du telephone de la derniere synchro 
+		
+		
+		
 	});
 
 }
-
 // Declaration de la methode qui appelle la methode de selection (du modele(dao))
 function recupererDetailPisteControleur(nomPiste) {
 	alert("dans recuperer..");
@@ -34,16 +39,50 @@ function recupererDetailPisteControleur(nomPiste) {
 }
 
 
-// Fonction de callback onSuccess, reçoit un objet Position
-//
-function onSuccess(position) {
+// affichage d'une liste de piste dans results suite à appel select dans la page
+function AfficherListePiste(tx,results) {
+alert("dans afficher liste");
+	var len = results.rows.length;
+	lesPistes = new Array(len);
+
+	alert ("boucle for" + len);
+	if (len > 0) {
+		$('#liste_pistes li').remove();
+		for (i = 0; i < len; i++) {
+			var index = i;
+			var divStar = "star" + index;
+
+			$('#liste_pistes').append('<li><div class="piste"><div class="photo"><img src="' + results.rows.item(i).Photo +'" alt="Piste" width="50px" height="50px"></div>' +
+					'<div class="texte"><h2><a href="detailPiste.html?piste.idPiste=' + results.rows.item(i).PisteId + '">' + results.rows.item(i).Nom + 
+					'<div style="background-color:'+results.rows.item(i).Couleurd+'" class="couleur img-circle"></div></h2></div>' +
+					'<div class="note_globale">' + 
+					'<span>Note </span> <strong>' + results.rows.item(i).NotGlob + '</strong></div>' + 
+					'<div id="' + divStar  + '" data-score="' + results.rows.item(i).NotGlob + '" disabled="disabled">' +
+			'</div></div>');
+			$("#" + divStar ).raty({
+				readOnly  : true,
+				width: false,
+				path : "./images/",
+				score: function() {
+					return $(this).attr('data-score');
+				}
+			});
+		}
+	}
+
+	$('#liste_pistes').listview('refresh');
+
+}
+
+//Fonction de callback onSuccess, reçoit un objet Position
+
+function creationBDD(position) {
+	
 	alert("votre position : longitude " + position.coords.longitude +"\nlatitude : " + position.coords.latitude);
 	var listPisteSeolan = getPisteList(latitude, longitude);
 	initbdd(listPisteSeolan);
-	//listPistAll();
-	//alert("tableau à afficher : " + lesPistes.length);
+	listPistAll();
 
-	
 }
 
 //Fonction de callback onError, reçoit un objet PositionError
