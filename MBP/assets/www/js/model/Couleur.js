121e150b393773c18589184b623fe28e8655dbed
valerieP <valerie.pepoz@xsalto.com>
2013-07-29 17:32:03 +0200

    Ajout des classe javascript : Couleur, Massif et Station

---
+++ MBP/assets/www/js/model/Couleur.js
@@ -0,0 +1,13 @@
+function Couleur(id, oid, libelle, couleur)
+{
+	 var id ;
+	 var oid ;
+	 var libelle ;
+	 var couleur ;
+	 
+	 // Constructeur
+	 this.id = id;
+	 this.oid = oid;
+	 this.libelle = libelle;
+	 this.couleur = couleur;
+ }
---
+++ MBP/assets/www/js/model/Massif.js
@@ -0,0 +1,15 @@
+function Massif(id, oid, nom, statut, descr)
+ {
+	 var id ;
+	 var oid ;
+	 var nom ;
+	 var statut ;
+	 var descr ;
+
+	 // Constructeur
+	 this.id = id;
+	 this.oid = oid;
+	 this.nom = nom;
+	 this.statut = statut;
+	 this.descr = descr;
+ }
