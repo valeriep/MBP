3f6930ce4258efc77108689dfc9a9faf87cd3b27
valerieP <valerie.pepoz@xsalto.com>
2013-07-29 17:31:19 +0200

    ajout des massifs, stations, couleurs

--- MBP/assets/www/js/view/nouvellePiste.js
+++ MBP/assets/www/js/view/nouvellePiste.js
@@ -18,18 +18,25 @@ function insererCodeNouvellePiste(){
 			'<form action="#" method="post">'+
 			 
 				'<fieldset data-role="fieldcontain">'+
-					'<label for="username">Nom de la piste</label>'+
+					'<label for="username">Nom de la piste *</label>'+
 					'<input type="text" name="nom_nouvelle_piste" id="nom_nouvelle_piste">'+
 				'</fieldset>'+
 				 
 				'<fieldset data-role="fieldcontain">'+
-					'<label for="favcolor">Favorite Color:</label>'+
-					'<select id="favcolor" name="favcolor">'+
-						'<option>Select One</option>'+
-						'<option>Green</option>'+
-						'<option>Red</option>'+
-						'<option>Blue</option>'+
-						'<option>Yellow</option>'+
+					'<label for="massif">Massif *</label>'+
+					'<select class="massifSelected" id="massif" name="massif">'+
+					'</select>'+
+				'</fieldset>'+
+
+				'<fieldset data-role="fieldcontain">'+
+					'<label for="station">Station *</label>'+
+					'<select class="stationSelected" id="station" name="station">'+
+					'</select>'+
+				'</fieldset>'+
+			
+				'<fieldset data-role="fieldcontain">'+
+					'<label for="couleur">Favorite Color:</label>'+
+					'<select id="couleur" name="couleur">'+
 					'</select>'+
 				'</fieldset>'+
 				 
@@ -41,5 +48,29 @@ function insererCodeNouvellePiste(){
 				'<input type="submit" value="Register">'+
 				 
 			'</form>');
+	afficherContenueNouvellePiste();
+	
+	$('.massifSelected').change(function() {
+		var indexMassifSelected = document.getElementById('massif').selectedIndex;
+		
+		if(indexMassifSelected !=0) {
+			// on appelle la fonction qui affiche les stations correspondanntes au massif selectionné
+			recupererListeStationDUnMassif(document.getElementById('massif').value);
+		}
+		else {
+			// On recupère toutes les stations
+			recupererListeAllStations();
+		}
+	});
+	
+	$('.stationSelected').change(function() {
+		var indexMassifSelected = document.getElementById('massif').selectedIndex;
+		
+		// si on n'avait pas encore selectionné le massif:
+		if(indexMassifSelected == 0) {
+			// on appelle la fonction qui affiche les stations correspondanntes au massif selectionné
+			recupererListeMassifDUneStation(document.getElementById('station').value);
+		}
+	});
 	
 }
