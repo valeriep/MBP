874825fc1c0013c4fd493f3dc28bda0fc3219da7
Ch4mp <ch4mp@c4-soft.com>
2013-11-11 15:52:41 +0100

    Create piste detail widget

---
+++ assets/www/js/view/PisteDetailWidget.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * Piste detail Widget
+ * 
+ * @constructor
+ * @author Ch4mp
+ * 
+ */
+mbp.PisteDetailWidget = function() {
+    mbp.Widget.call(this, '#dot-mark-snippet, #dot-piste-detail');// parent constructor
+    var parentDisplay = this.display;// save reference to Widget display function to call it from overloading function
+
+    /**
+     * Triggers Widget display and registers UI & form event handlers
+     * @param {mb.Piste} piste
+     */
+    this.display = function(piste) {
+        parentDisplay.call(this, piste);
+    };
+
+    Object.preventExtensions(this);
+};
---
+++ test/view/PisteDetailWidgetTest.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var piste1 = null;
+
+module('PisteDetailWidget', {
+    setup : function() {
+        jQuery('div[data-role="content"]').html('');
+        var testResort = new mbp.Resort('testResortId', 'Test Resort', 'Test Country', 'Test Massif');
+        piste1 = new mbp.Piste('testPiste1', 'Test Piste 1', 'black', 'Black test piste', 'img/piste/testPiste1.jpg', 4, testResort);
+        new mbp.Comment('testComment1', 'First comment on piste 1', 4, 1, piste1);
+        new mbp.Comment('testComment2', 'Second comment on piste 1', 5, 1, piste1);
+    },
+    teardown : function() {
+        jQuery('div[data-role="content"]').html('');
+    }
+});
+test('widget is displayed in div with data-role="content"', function() {
+    var widget = new mbp.PisteDetailWidget();
+    widget.display(piste1);
+    equal(jQuery('div[data-role="content"] .pisteComment').length, 2);
+});
