90d1944831a5cb40cc54e68348a821d52cfcc683
Ch4mp <ch4mp@c4-soft.com>
2013-11-14 09:41:47 +0100

    Bind new pist form to a backing object

--- www/js/view/NewPisteWidget.js
+++ www/js/view/NewPisteWidget.js
@@ -12,18 +12,23 @@ mbp.NewPisteWidget = function(onSubmit) {
     mbp.Widget.call(this, '#dot-new-piste');// parent constructor
     var parentDisplay = this.display;// save reference to Widget display function to call it from overloading function
 
-    /**
-     * Triggers Widget display and registers UI & form event handlers
-     * @param {mbp.User} user User to authenticate. If provided it is used to fill "username" field
-     */
     this.display = function() {
         parentDisplay.call(this);
-        $('.newPisteForm').submit(function() {
-            onSubmit($('#name').val());
-            return false; // interrupt submit chain
-        });
-        $('.newPisteForm .savePiste').click(function() {
-            $('.newPisteForm').submit();
+        $('#newPisteForm').submit(
+                function() {
+                    var newPiste = new mbp.NewPiste(
+                            $('#country').val(),
+                            $('#massif').val(),
+                            $('#resort').val(),
+                            $('#name').val(),
+                            $('#color').val(),
+                            $('#description').val(),
+                            $('#keywords').val());
+                    onSubmit(newPiste);
+                    return false; // interrupt submit chain
+                });
+        $('#newPisteForm .savePiste').click(function() {
+            $('#newPisteForm').submit();
         });
     };
 
