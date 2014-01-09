61bff1e1d3ddd1b6fd392b7ee534e8c57b0e2e4d
ch4mpy <ch4mp@c4-soft.com>
2014-01-09 22:05:49 +0100

    Create ColorSelectionWidget tests

---
+++ test/widget/ColorSelectionWidgetTest.js
@@ -0,0 +1,29 @@
+"use strict";
+
+module('ColorSelectionWidget', {
+	setup : function() {
+		jQuery('#content').html('');
+	},
+	teardown : function() {
+		jQuery('#content').html('');
+	}
+});
+test('widget is displayed in element specified by jQuerySelector', function() {
+	var firstValueChangedCall = true;
+	var widget = new mbp.ColorSelectionWidget('#content', true, function(selectedColor) {
+		if(firstValueChangedCall) {
+			firstValueChangedCall = false;
+			equal(selectedColor, '');
+		} else {
+			equal(selectedColor, mbp.Piste.BLACK);
+		}
+	});
+
+	expect(2);
+
+	widget.show(mbp.Piste.COLORS);
+
+	jQuery('#color').val(mbp.Piste.BLACK);
+	jQuery('#color').selectmenu("refresh");
+	jQuery('#color').trigger('change');
+});
