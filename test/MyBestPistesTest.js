e1d94e2986d3364e910400814adfad7270bffc00
ch4mpy <ch4mp@c4-soft.com>
2014-01-15 20:09:51 +0100

    Reactivate all tests

--- test/MyBestPistesTest.js
+++ test/MyBestPistesTest.js
@@ -1,9 +1,12 @@
 "use strict";
 
+var app;
+
 module("MyBestPistes", {
     setup : function() {
         localStorage.clear();
         app = new mbp.MyBestPistes();
+        app.seolanRepo = new mbp.StubSeolanRepository();
         app.save();
         jQuery('#content').empty();
         jQuery('div[data-role="footer"]').empty();
@@ -15,17 +18,17 @@ module("MyBestPistes", {
     }
 });
 test("load() retrieves User and session Id", function() {
-    localStorage.setItem('mbp.username', 'ch4mp');
-    new mbp.LocalUserRepository().save(new mbp.User('U1', 'ch4mp', 'toto', 'test'));
+    localStorage.setItem('mbp.userId', 'U1');
+    new mbp.LocalUserRepository().save(mbp.User.build('U1', 'ch4mp', 'toto', 'test'));
     app.load();
     equal(app.user.login, 'ch4mp');
     ok(!app.user.pwd); // password is not persisted
     equal(app.user.sessionId, 'test');
 });
 test("load() creates new User with persisted username even if user is not persistent (username saved in app state, but user not accessible by LocalUserRepository)", function() {
-    localStorage.setItem('mbp.username', 'ch4mp');
+    localStorage.setItem('mbp.userId', 'U1');
     app.load();
-    equal(app.user.login, 'ch4mp');
+    equal(app.user.login, 'U1');
     ok(!app.user.pwd);
     ok(!app.user.sessionId);
 });
@@ -34,8 +37,8 @@ test("load() inits app with null user if app has no persistent state (i.e. first
     strictEqual(app.user, null);
 });
 test("load() persisted user with session id skips authentication form display", function() {
-    localStorage.setItem('mbp.username', 'ch4mp');
-    new mbp.LocalUserRepository().save(new mbp.User('U1', 'ch4mp', 'toto', 'test'));
+    localStorage.setItem('mbp.userId', 'U1');
+    new mbp.LocalUserRepository().save(mbp.User.build('U1', 'ch4mp', 'toto', 'test'));
     app.load();
     ok(!jQuery('#login-form').html());
     jQuery(document).ready(function() {
@@ -43,7 +46,7 @@ test("load() persisted user with session id skips authentication form display", 
     });
 });
 test("unload() creates app persistence (saves username)", function() {
-    app.user = new mbp.User('U1', 'ch4mp');
+    app.user = mbp.User.build('U1', 'ch4mp');
     app.unload();
-    equal(localStorage.getItem('mbp.username'), 'ch4mp');
+    equal(localStorage.getItem('mbp.userId'), 'U1');
 });
